e8e17d2bc8e989a5f150da05891de83e958bbcc7
gbrb1 <gbrb1@hotmail.com>
2021-04-01 21:20:22 -0300

    Adicionando swagger com rotas de exemplo

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("Exemplo")
+@ApiTags('Exemplo1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/hello")
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/exemplo")
+  getExemplo() {
+    return this.appService.getExemplo();
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,19 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(bodyParser.json({ limit: '50mb' }));
+  
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('Projeto Web3')
+    .setDescription('API para consumir simultaneamente recursos da API do tweeter e do google maps')
+    .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
